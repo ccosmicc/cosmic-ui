343c6c41b214e15aad76b91e54f65e15ec8aeadc
haticecs <thisishatice@gmail.com>
2021-11-29 18:11:44 +0300

    firebase config added

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDSqBLTAWEOse2dxEmotpEA3YkLflK-4-4",
+  authDomain: "cosmic-a5068.firebaseapp.com",
+  projectId: "cosmic-a5068",
+  storageBucket: "cosmic-a5068.appspot.com",
+  messagingSenderId: "470389855236",
+  appId: "1:470389855236:web:cea87d757ea50256fa9151",
+};
